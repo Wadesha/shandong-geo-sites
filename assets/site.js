91c5649f95623e86b26443d1fe937f7b64efcb34
Wadesha <zhuchenshao@gmail.com>
2026-09-13 20:28:24 +0800

    山东地质实地考察点：50 处点位 / 9 区段 / 15 原理 多页静态站

--- assets/site.js
+++ assets/site.js
@@ -40,7 +40,10 @@
     var html = '<p class="vt">' + d.n + "</p>" +
       '<p class="psub">' + d.g + " ｜ " + d.loc + "</p>" +
       '<p class="q">' + d.s + "</p>" +
-      '<p class="kvline">时代：' + d.age + " ｜ 原理标签：" + why + "</p>";
+      (d.o ? '<p class="kvline">现场看点：' + d.o + "</p>" : "") +
+      '<p class="kvline">时代：' + d.age + " ｜ 本页正文约 " + (d.wc || 0) + " 字" +
+      (d.bk ? "，附志书原文 " + d.bk + " 段" : "") +
+      " ｜ 术语 " + (d.tm || 0) + " 条 ｜ 原理标签：" + why + "</p>";
     if (v) { v.innerHTML = html; }
     var o = document.getElementById("btnopen");
     if (o) { o.href = PREFIX + "t/" + d.slug + ".html"; }
